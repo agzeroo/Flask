7e9825152b2f057c2450bcf132eb5d593d36039a
agzroo <kibudy2@gmail.com>
2021-08-10 11:22:39 +0900

    edit

--- flask_cms/run.py
+++ flask_cms/run.py
@@ -1,35 +1,25 @@
-# Flask 기본 템플릿
-
-# 1. 모듈 가져오기
+# 기본 템플릿
 from flask import Flask, render_template
 
-# 2. 앱 생성 (flask 객체 생성)
 app = Flask(__name__)
-
-# 3. 라우팅
-
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
 @app.route('/formUpload')
 def formUpload():
-    return render_template('formUpload.html')
-
+    return render_template('pages/formUpload.html')
 
 @app.route('/ajaxUpload')
 def ajaxUpload():
-    return render_template('ajaxUpload.html')
-
+    return render_template('pages/ajaxUpload.html')
 
 @app.route('/webcamUpload')
 def webcamUpload():
-    return render_template('webcamUpload.html')
+    return render_template('pages/webcamUpload.html')
 
 
-# 4. 서버 가동
 if __name__ == '__main__':
     app.run(debug=True)
     pass
